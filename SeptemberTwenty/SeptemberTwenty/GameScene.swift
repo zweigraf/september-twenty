5cf7cdf7f9c4c9b5847a5b6bd87d46ba700accea
Luis Reisewitz <zweigraf@Luiss-MacBook-Pro.local>
2015-09-20 20:59:35 +0200

    add Paddle hero block and let it move with finger position

--- SeptemberTwenty/SeptemberTwenty/GameScene.swift
+++ SeptemberTwenty/SeptemberTwenty/GameScene.swift
@@ -9,18 +9,35 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    let PaddleColor = UIColor.redColor().colorWithAlphaComponent(0.5)
+    let PaddleSize = CGSize(width: 200, height: 50)
+    let PaddleBottomOffset = 50
+    
+    let PaddleName = "paddle"
+    
+    weak var paddle : SKNode?
+    
     override func didMoveToView(view: SKView) {
         scaleMode = .AspectFit
         addGrid()
+        
+        addPaddle()
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
        /* Called when a touch begins */
         
-        for touch in touches {
+        if let touch = touches.first {
             let location = touch.locationInNode(self)
             
-            print(location)
+            setPaddlePosition(location.x)
+        }
+    }
+    
+    override func touchesMoved(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        if let touch = touches.first {
+            let location = touch.locationInNode(self)
+            setPaddlePosition(location.x)
         }
     }
    
@@ -32,5 +49,16 @@ class GameScene: SKScene {
         let grid = GridGenerator.createGrid(self.frame, step: 50)
         grid.zPosition = -1
         addChild(grid)
+    }
+    
+    func addPaddle() {
+        let paddle = SKSpriteNode(color: PaddleColor, size: PaddleSize)
+        paddle.position = CGPoint(x: Int(CGRectGetMidX(self.frame)), y: PaddleBottomOffset)
+        addChild(paddle)
+        self.paddle = paddle
+    }
+    
+    func setPaddlePosition(x:CGFloat) {
+        paddle?.position.x = x
     }
 }
