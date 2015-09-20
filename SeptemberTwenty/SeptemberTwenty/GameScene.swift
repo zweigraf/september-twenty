41bcb0f455698c6ad9eb2d567ee989fb026f8096
Luis Reisewitz <zweigraf@Luiss-MacBook-Pro.local>
2015-09-20 20:59:58 +0200

    Add rows of blocks to be broken to the top of the scene

--- SeptemberTwenty/SeptemberTwenty/GameScene.swift
+++ SeptemberTwenty/SeptemberTwenty/GameScene.swift
@@ -10,10 +10,19 @@ import SpriteKit
 
 class GameScene: SKScene {
     let PaddleColor = UIColor.redColor().colorWithAlphaComponent(0.5)
+    let BlockColor = UIColor.greenColor().colorWithAlphaComponent(0.5)
+    
     let PaddleSize = CGSize(width: 200, height: 50)
+    let BlockSize = CGSize(width: 100, height: 50)
+    
     let PaddleBottomOffset = 50
+    let BlockTopOffset = 68
+    let BlockLeftOffset : CGFloat = 50
+    let BlockRightOffset : CGFloat = 50
+    let BlockRowCount = 4
     
     let PaddleName = "paddle"
+    let BlockName = "block"
     
     weak var paddle : SKNode?
     
@@ -22,6 +31,7 @@ class GameScene: SKScene {
         addGrid()
         
         addPaddle()
+        addBlocks()
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -56,6 +66,27 @@ class GameScene: SKScene {
         paddle.position = CGPoint(x: Int(CGRectGetMidX(self.frame)), y: PaddleBottomOffset)
         addChild(paddle)
         self.paddle = paddle
+    }
+    
+    func addBlocks() {
+        let space = self.frame.size.width - BlockLeftOffset - BlockRightOffset
+        
+        let numberOfBlocksInRow = Int(space / BlockSize.width)
+        let widthOfRow = CGFloat(numberOfBlocksInRow) * BlockSize.width
+        let xOfFirstBlock = self.frame.origin.x + BlockLeftOffset + ((space - widthOfRow) / 2) + (BlockSize.width / 2)
+        let xOfLastBlock = xOfFirstBlock + (CGFloat(numberOfBlocksInRow - 1) * BlockSize.width)
+        
+        let yOfFirstBlock = CGRectGetMaxY(self.frame) - CGFloat(BlockTopOffset) - (BlockSize.height / 2)
+        let yOfLastBlock = yOfFirstBlock - (CGFloat(BlockRowCount - 1) * BlockSize.height)
+        
+        for var y = yOfFirstBlock; y >= yOfLastBlock; y -= BlockSize.height {
+            for var x = xOfFirstBlock; x <= xOfLastBlock; x += BlockSize.width {
+                let block = SKSpriteNode(color: BlockColor, size: BlockSize)
+                block.position = CGPoint(x: x, y: y)
+                block.name = BlockName;
+                addChild(block)
+            }
+        }
     }
     
     func setPaddlePosition(x:CGFloat) {
